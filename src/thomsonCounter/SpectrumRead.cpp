0ea78e7ccc8db4051fede09c64d1373b9ed7d968
user <vitas02@list.ru>
2026-03-29 22:09:45 +0700

    convolution work with out files

--- src/thomsonCounter/SpectrumRead.cpp
+++ src/thomsonCounter/SpectrumRead.cpp
@@ -114,6 +114,7 @@ bool readSpectrumFromT(std::string filename, double &T0, double &dT, uint &N_T, 
     if (N_T == 0) //если нет данных значит начальная темпераутра 100 эВ для всех
     {
         N_T = 1;
+        std::cerr << "начальная температура: " << T0 << " эВ\n";
         T0 = DEFAULT_T0;
         dT = 0.;
         S_T.resize(N_CHANNELS, 0.);
